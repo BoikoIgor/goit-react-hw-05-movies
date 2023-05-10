2cfc565c21bd7ce56fe6434d938a97a5068fa73c
Igor Boiko <igorboiko@gmail.com>
2023-05-10 15:38:59 +0300

    fixed NotFound css

--- src/components/NotFound/NotFound.styled.js
+++ src/components/NotFound/NotFound.styled.js
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 export const NotFoundBlock = styled.div`
   margin: 1rem auto;
   font-weight: 500;
-  height: 20rem;
+  text-align: center;
+  height: 50vh;
+  width: 100vh;
   background-size: cover;
   background-position: center;
   background-image: url('https://img.freepik.com/premium-photo/sos-sign-with-gold-lifebuoy_427873-678.jpg?w=2000');
